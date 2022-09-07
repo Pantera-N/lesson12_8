d53b498a7198b6d24d305a5f7144f95cb1198b97
Lena <natalyapiano19@mail.ru>
2022-09-07 21:53:19 +0300

    file_1

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/users/<uid>')
-def profile(uid):
-    return f'<h1>Профиль {uid}</h1>'
+@app.route('/')
+def page_index():
+    return f'Главная страничка'
 
 
-@app.route('/catalog/items/<itemid>')
-def profile(itemid):
-    return f'<h1>Страничка товара {itemid}</h1>
+@app.route('/profile/<int:user_id>')
+def page_profile(user_id):
+    return f'Страничка пользователя {user_id}'
+
+app.run()
 
